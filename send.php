2530db51c36b71e6a5baa524c6f59b142b6256f9
islam136 <islamnizamov25@gmail.com>
2025-03-24 13:06:42 +0600

    edit send

--- send.php
+++ send.php
@@ -1,8 +1,8 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 $data = json_decode(file_get_contents("php://input"), true);
     
